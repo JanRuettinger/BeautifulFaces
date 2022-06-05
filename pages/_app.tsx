dac39506b88316474c6ed64a04e33a139224a83e
Jan Ruettinger <service+github@janruettinger.com>
2022-06-05 01:14:08 -0700

    add connect wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,57 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import {
+    WagmiConfig,
+    createClient,
+    defaultChains,
+    configureChains,
+} from 'wagmi';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+
+const alchemyId = process.env.ALCHEMY_KEY;
+
+const { chains } = configureChains(defaultChains, [
+    alchemyProvider({ alchemyId }),
+    publicProvider(),
+]);
+
+const client = createClient({
+    autoConnect: true,
+    connectors: [
+        new MetaMaskConnector({ chains }),
+        new CoinbaseWalletConnector({
+            chains,
+            options: {
+                appName: 'wagmi',
+            },
+        }),
+        new WalletConnectConnector({
+            chains,
+            options: {
+                qrcode: true,
+            },
+        }),
+        // new InjectedConnector({
+        //     chains,
+        //     options: {
+        //         name: 'Injected',
+        //         shimDisconnect: true,
+        //     },
+        // }),
+    ],
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <WagmiConfig client={client}>
+            <Component {...pageProps} />
+        </WagmiConfig>
+    );
 }
-
-export default MyApp
